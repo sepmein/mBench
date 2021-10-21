4d1c175c34aec509c01b1e0ced0e4a0d243d470d
sepmein <sepmein@gmail.com>
2021-10-21 23:13:43 +0200

    yes

--- imperial_deterministic.r
+++ imperial_deterministic.r
@@ -6,8 +6,8 @@ params <-
     model_param_list_create()
 params$eta <- 1 / (21.2 * 365)
 params$init_EIR <- 1280 # nolint
-ghana <- run_model(
-    country = "Ghana",
-    init_EIR = 1
-)
-out$plot
+ghana <- run_model()
+plot(ghana$t, ghana$prev, main = "Prevalance", ylim = c(0, 1), type = "l")
+
+# seasonal data
+# resource: 
