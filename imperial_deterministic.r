9ffcbdd286b0b7d8834d3364d8edbc954a8ae0d8
sepmein <sepmein@gmail.com>
2021-11-24 15:44:44 +0100

    updated R script for Imperial Colleage

--- imperial_deterministic.r
+++ imperial_deterministic.r
@@ -2,12 +2,30 @@ library(ICDMM,
     help,
     pos = 2, lib.loc = NULL
 )
-params <-
-    model_param_list_create()
-params$eta <- 1 / (21.2 * 365)
-params$init_EIR <- 1280 # nolint
-ghana <- run_model()
-plot(ghana$t, ghana$prev, main = "Prevalance", ylim = c(0, 1), type = "l")
-
 # seasonal data
-# resource: 
+# resource:
+library(gdata)
+
+gha_params <- read.csv("./Data/GHA/gha.csv")
+gha_params$adm1
+for (row in 1:nrow(gha_params)) {
+    eta <- gha_params[row, "eta"]
+    rho <- gha_params[row, "rho"]
+    eir <- gha_params[row, "eir"]
+    province <- gha_params[row, "adm1"]
+    treatment_seeking <- gha_params[row, "treatment_seeking"]
+    # TODO
+    adm1 <- run_model(
+        init_EIR = eir,
+        country = "Ghana",
+        admin2 = province,
+        time = 365 * 3,
+        init_ft = treatment_seeking
+    )
+    plot(adm1$t,
+        adm1$prev,
+        main = "Prevalance",
+        ylim = c(0, 1),
+        type = "l"
+    )
+}
