18304a0396424d811155b2cc86daaacb4390e392
sepmein <sepmein@gmail.com>
2022-03-02 20:50:00 +0100

    improved modelling output plot and imperial simualtion.

--- scripts/imperial_deterministic.r
+++ scripts/imperial_deterministic.r
@@ -11,6 +11,7 @@ library(ICDMM,
 library(tidyverse)
 library(ggplot2)
 library(gdata)
+library("gridExtra")
 
 gha_params <-
     read.csv("/Users/sepmein/Dropbox/benchmarking/Data/GHA/gha.csv")
@@ -21,13 +22,29 @@ cases_reduction_with_pbo_same_coverage <- c()
 cases_reduction_with_pbo_same_budget_global_fund <- c()
 cases_reduction_with_pbo_same_budget_ellie_2022 <- c()
 
+im_year <- c()
+im_adm1 <- c()
+im_cases_no_net <- c()
+im_cases_net <- c()
+im_cases_pbo <- c()
+im_cases_pbo_gf <- c()
+im_cases_pbo_ellie <- c()
+
+om_year <- c()
+om_adm1 <- c()
+om_cases_net <- c()
+om_cases_pbo <- c()
+om_cases_pbo_gf <- c()
+om_cases_pbo_ellie <- c()
+
+plot_export_path <- "/Users/sepmein/Dropbox/benchmarking/Results/PBO vs LLINs/"
+imperial_plots_list <- list()
 for (row in 1:nrow(gha_params)) {
     eta <- gha_params[row, "eta"]
     rho <- gha_params[row, "rho"]
     eir <- gha_params[row, "eir"]
     province <- gha_params[row, "adm1"]
     old_province <- gha_params[row, "old_district_name"]
-    # province <- gha_params[row, "adm1"]
     itn_cov <- gha_params[row, "itn_cov"]
     pbo_cov_gf <- gha_params[row, "pbo_cov_gf"]
     pbo_cov_ellie <- gha_params[row, "pbo_cov_ellie"]
@@ -47,7 +64,7 @@ for (row in 1:nrow(gha_params)) {
         init_EIR = eir,
         country = "Ghana",
         admin2 = old_province,
-        time = 365 * 3 + 200,
+        time = 365 * 4,
         init_ft = treatment_seeking
     )
     # plot(adm1_without_intervention$t,
@@ -62,7 +79,7 @@ for (row in 1:nrow(gha_params)) {
         init_EIR = eir,
         country = "Ghana",
         admin2 = old_province,
-        time = 365 * 3 + 200,
+        time = 365 * 4,
         num_int = 3,
         itn_cov = itn_cov,
         irs_cov = irs_cov,
@@ -84,7 +101,7 @@ for (row in 1:nrow(gha_params)) {
         init_EIR = eir,
         country = "Ghana",
         admin2 = old_province,
-        time = 365 * 3 + 200,
+        time = 365 * 4,
         num_int = 3,
         itn_cov = itn_cov,
         irs_cov = irs_cov,
@@ -107,7 +124,7 @@ for (row in 1:nrow(gha_params)) {
         init_EIR = eir,
         country = "Ghana",
         admin2 = old_province,
-        time = 365 * 3 + 200,
+        time = 365 * 4,
         num_int = 3,
         itn_cov = pbo_cov_gf,
         irs_cov = irs_cov,
@@ -131,7 +148,7 @@ for (row in 1:nrow(gha_params)) {
         init_EIR = eir,
         country = "Ghana",
         admin2 = old_province,
-        time = 365 * 3 + 200,
+        time = 365 * 4,
         num_int = 3,
         itn_cov = pbo_cov_ellie,
         irs_cov = irs_cov,
@@ -200,52 +217,46 @@ for (row in 1:nrow(gha_params)) {
     # print(price_with_net / n_reduced_cases_net)
     # print("$ per averted cases with pbo")
     # print(price_with_pbo / n_reduced_cases_pbo)
-    date <- adm1_without_intervention$t
-    without_intervention <- adm1_without_intervention$prev
-    with_net <- adm1$prev
-    with_pbo <- adm1_switching_to_PBO$prev
-    with_pbo_gf <- adm1_switching_to_PBO_same_budget_gf$prev
-    with_pbo_ellie <- adm1_switching_to_PBO_same_budget_ellie$prev
+    year <- adm1_without_intervention$t / 365
+    without_intervention <- adm1_without_intervention$prev * total_population / 1000000
+    with_net <- adm1$prev * total_population / 1000000
+    with_pbo <- adm1_switching_to_PBO$prev * total_population / 1000000
+    with_pbo_gf <- adm1_switching_to_PBO_same_budget_gf$prev * total_population / 1000000
+    with_pbo_ellie <- adm1_switching_to_PBO_same_budget_ellie$prev * total_population / 1000000
 
+    im_year <- append(im_year, year)
+    im_adm1 <- append(im_adm1, rep(province, length(year)))
+    im_cases_no_net <- append(im_cases_no_net, without_intervention)
+    im_cases_net <- append(im_cases_net, with_net)
+    im_cases_pbo <- append(im_cases_pbo, with_pbo)
+    im_cases_pbo_gf <- append(im_cases_pbo_gf, with_pbo_gf)
+    im_cases_pbo_ellie <- append(im_cases_pbo_ellie, with_pbo_ellie)
     prevalance <- data.frame(
-        date,
+        year,
         without_intervention,
         with_net,
         with_pbo,
         with_pbo_gf,
         with_pbo_ellie
     )
-    regional_plot_prevalence_pbo_same_coverage <- ggplot(
+    im_plot_adm1 <- ggplot(
         data = prevalance,
-        aes(x = date)
+        aes(x = year)
     ) +
-        geom_line(aes(y = without_intervention, color = "No Net")) +
-        geom_line(aes(y = with_net, color = "Pyrethroid Only", linetype = "dashed")) +
+        geom_line(aes(
+            y = without_intervention,
+            color = "No Net"
+        )) +
+        geom_line(aes(
+            y = with_net,
+            color = "Pyrethroid Only",
+            linetype = "dashed"
+        )) +
         geom_line(aes(
             y = with_pbo,
             color = "PBO",
-            linetype = "twodash"
+            linetype = "dashed"
         )) +
-        scale_color_manual(
-            name = "Intervention",
-            values = c("No Net" = "grey69", "Pyrethroid Only" = "darkred", "PBO" = "steelblue")
-        ) +
-        guides(linetype = "none")+
-        ggtitle(
-            paste(
-                "Region: ",
-                province,
-                ", Malaria Prevalences - same coverage"
-            )
-        )
-    show(regional_plot_prevalence_pbo_same_coverage)
-
-    regional_plot_prevalence_pbo_same_budget <- ggplot(
-        data = prevalance,
-        aes(x = date)
-    ) +
-        geom_line(aes(y = without_intervention, color = "No Net")) +
-        geom_line(aes(y = with_net, color = "Pyrethroid Only", linetype = "dashed")) +
         geom_line(aes(
             y = with_pbo_gf,
             color = "PBO_GF",
@@ -258,19 +269,91 @@ for (row in 1:nrow(gha_params)) {
         )) +
         scale_color_manual(
             name = "Intervention",
-            values = c("No Net" = "grey69", "Pyrethroid Only" = "darkred", "PBO_GF" = "steelblue", "PBO_E" = "#099009")
+            values = c(
+                "No Net" = "grey69",
+                "Pyrethroid Only" = "darkred",
+                "PBO" = "steelblue",
+                "PBO_GF" = "#b56d02",
+                "PBO_E" = "#099009"
+            )
         ) +
-        guides(linetype = "none")+
+        ylab("Cases in Millions") +
+        guides(linetype = "none") +
         ggtitle(
             paste(
-                "Region: ",
+                "IM - ",
                 province,
-                ", Prevalences comparison - at the same budget"
+                "- PBO vs LLINs"
             )
         )
-    show(regional_plot_prevalence_pbo_same_budget)
+    show(im_plot_adm1)
+    ggsave(filename = paste(
+        "IM -",
+        province,
+        ".png"
+    ), path = plot_export_path)
+    imperial_plots_list <- append(imperial_plots_list, im_plot_adm1)
 }
 
+im_output <- data.frame(
+    im_year,
+    im_adm1,
+    im_cases_no_net,
+    im_cases_net,
+    im_cases_pbo,
+    im_cases_pbo_gf,
+    im_cases_pbo_ellie
+)
+
+ggplot(
+    data = im_output,
+    aes(x = im_year)
+) +
+    geom_line(aes(
+        y = im_cases_no_net,
+        color = "No Net"
+    )) +
+    geom_line(aes(
+        y = im_cases_net,
+        color = "Pyrethroid Only",
+        linetype = "dashed"
+    )) +
+    geom_line(aes(
+        y = im_cases_pbo,
+        color = "PBO",
+        linetype = "dashed"
+    )) +
+    geom_line(aes(
+        y = im_cases_pbo_gf,
+        color = "PBO_GF",
+        linetype = "twodash"
+    )) +
+    geom_line(aes(
+        y = im_cases_pbo_ellie,
+        color = "PBO_E",
+        linetype = "twodash"
+    )) +
+    scale_color_manual(
+        name = "Intervention",
+        values = c(
+            "No Net" = "grey69",
+            "Pyrethroid Only" = "darkred",
+            "PBO" = "steelblue",
+            "PBO_GF" = "#b56d02",
+            "PBO_E" = "#099009"
+        )
+    ) +
+    ylab("Cases in Millions") +
+    xlab("Year") +
+    guides(linetype = "none") +
+    ggtitle(
+        "IM - PBO vs LLINs"
+    ) +
+    facet_wrap(. ~ im_adm1, scales = "free_y", ncol = 4)
+ggsave(
+    filename = "IM - PBO vs LLINs.png",
+    path = plot_export_path
+)
 
 output <- data.frame(
     gha_params["adm1"],
@@ -310,9 +393,12 @@ plot_with_pbo_same_coverage <- ggplot(
         hjust = 1
     )) +
     scale_fill_brewer(palette = "Blues") +
-    ggtitle("Cases averted - Switch to PBO - same coverage")
+    ggtitle("IM - Cases averted - Switch to PBO - Same Coverage")
 show(plot_with_pbo_same_coverage)
-
+ggsave(
+    filename = "IM - Cases Averted - Switch to PBO - Same Coverage.png",
+    path = plot_export_path
+)
 # cases reduction with PBO same budget
 plot_with_pbo_same_budget_ellie_2022 <- ggplot(
     data = output,
@@ -339,8 +425,13 @@ plot_with_pbo_same_budget_ellie_2022 <- ggplot(
         hjust = 1
     )) +
     scale_fill_brewer(palette = "Blues") +
-    ggtitle("Cases averted - Switch to PBO - same budgets")
+    ggtitle("IM - Cases averted - Switch to PBO - Same Budget")
 show(plot_with_pbo_same_budget_ellie_2022)
+ggsave(
+    filename =
+        "IM - Cases Averted - Switch to PBO - Same Budget.png",
+    path = plot_export_path
+)
 # relationship with resistance and cases averted
 plot_resistance_cases_averted <- ggplot(
     data = output,
@@ -355,3 +446,132 @@ plot_resistance_cases_averted <- ggplot(
 ) +
     geom_point()
 show(plot_resistance_cases_averted)
+
+# reading openmalaria output
+
+for (row in 1:nrow(gha_params)) {
+    province <- gha_params[row, "adm1"]
+    xml_output_df <- read.csv(
+        paste0(
+            "/Users/sepmein/Dropbox/benchmarking/Data/openmalaria/output_xml/",
+            province,
+            ".csv"
+        )
+    )
+    xml_output_plot <- ggplot(
+        data = xml_output_df,
+        aes(x = year)
+    ) +
+        geom_line(aes(
+            y = net,
+            color = "Pyrethroid Only",
+            linetype = "dashed"
+        )) +
+        geom_line(aes(
+            y = pbo,
+            color = "PBO",
+            linetype = "dashed"
+        )) +
+        geom_line(aes(
+            y = pbo_gf,
+            color = "PBO_GF",
+            linetype = "twodash"
+        )) +
+        geom_line(aes(
+            y = pbo_ellie,
+            color = "PBO_E",
+            linetype = "twodash"
+        )) +
+        scale_color_manual(
+            name = "Intervention",
+            values = c(
+                "Pyrethroid Only" = "darkred",
+                "PBO" = "steelblue",
+                "PBO_GF" = "#b56d02", "PBO_E" = "#099009"
+            )
+        ) +
+        ylab("Cases in Millions") +
+        guides(linetype = "none") +
+        ggtitle(
+            paste0(
+                "OM - ",
+                province,
+                "- Switch to PBO"
+            )
+        )
+    show(xml_output_plot)
+    ggsave(
+        filename = paste(
+            "OM -",
+            province,
+            ".png"
+        ),
+        path = plot_export_path
+    )
+    year <- xml_output_df$year
+    with_net <- xml_output_df$net
+    with_pbo <- xml_output_df$pbo
+    with_pbo_gf <- xml_output_df$pbo_gf
+    with_pbo_ellie <- xml_output_df$pbo_ellie
+    om_year <- append(om_year, year)
+    om_adm1 <- append(om_adm1, rep(province, length(year)))
+    om_cases_net <- append(om_cases_net, with_net)
+    om_cases_pbo <- append(om_cases_pbo, with_pbo)
+    om_cases_pbo_gf <- append(om_cases_pbo_gf, with_pbo_gf)
+    om_cases_pbo_ellie <- append(om_cases_pbo_ellie, with_pbo_ellie)
+}
+
+om_output <- data.frame(
+    om_year,
+    om_adm1,
+    om_cases_net,
+    om_cases_pbo,
+    om_cases_pbo_gf,
+    om_cases_pbo_ellie
+)
+
+
+ggplot(
+    data = om_output,
+    aes(x = om_year)
+) +
+    geom_line(aes(
+        y = om_cases_net,
+        color = "Pyrethroid Only",
+        linetype = "dashed"
+    )) +
+    geom_line(aes(
+        y = om_cases_pbo,
+        color = "PBO",
+        linetype = "dashed"
+    )) +
+    geom_line(aes(
+        y = om_cases_pbo_gf,
+        color = "PBO_GF",
+        linetype = "twodash"
+    )) +
+    geom_line(aes(
+        y = om_cases_pbo_ellie,
+        color = "PBO_E",
+        linetype = "twodash"
+    )) +
+    scale_color_manual(
+        name = "Intervention",
+        values = c(
+            "Pyrethroid Only" = "darkred",
+            "PBO" = "steelblue",
+            "PBO_GF" = "#b56d02",
+            "PBO_E" = "#099009"
+        )
+    ) +
+    ylab("Cases in Millions") +
+    xlab("Year") +
+    guides(linetype = "none") +
+    ggtitle(
+        "OM - PBO vs LLINs"
+    ) +
+    facet_wrap(. ~ om_adm1, scales = "free_y", ncol = 4)
+ggsave(
+    filename = "OM - PBO vs LLINs.png",
+    path = plot_export_path
+)
