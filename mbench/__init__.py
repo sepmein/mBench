d28e7c05ec132cbc1e7d3f6e74b51892d8ec544f
sepmein <sepmein@gmail.com>
2022-03-28 15:15:19 +0200

    defined class structure

--- mbench/__init__.py
+++ mbench/__init__.py
@@ -1,6 +1,7 @@
 import mbench.demographic
 import mbench.intervention
 import mbench.util
+from typing import Any
 
 
 class Mbench:
@@ -22,10 +23,46 @@ class Mbench:
 
 
 class Parameter:
+    def __init__(
+        self,
+        name: str,
+        default_value: Any,
+        aliases: list = [],
+    ) -> None:
+        self.name = name
+        self._value = default_value
+        self.aliases = aliases
+
+class Parameter_list:
     def __init__(self) -> None:
         pass
 
+class Demography_Parameter_List(Parameter_list):
+    def __init__(self) -> None:
+        super().__init__()
+
+class Entomology_Parameter_List(Parameter_list):
+    def __init__(self) -> None:
+        super().__init__()
+class Intervention_Parameter_List(Parameter_list):
+    def __init__(self) -> None:
+        super().__init__()
+class Monitoring_Parameter_List(Parameter_list):
+    def __init__(self) -> None:
+        super().__init__()
+class HealthSystem_Parameter_List(Parameter_list):
+    def __init__(self) -> None:
+        super().__init__()
 class Model:
+    def __init__(self, name:str) -> None:
+        pass
+
+
+class Scenario:
     def __init__(self) -> None:
         pass
 
+
+class PredefinedScenario:
+    def __init__(self) -> None:
+        pass
